9a7340cb3cd3ba9da356626b9a745a9bc65fed5d
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-11-13 16:54:28 -0500

    created combat controller

---
+++ Assets/Scripts/Tank/TankCombatController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankCombatController : MonoBehaviour
+{
+    private TankControllerState state;
+    private TankConfig config;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = GetComponent<TankControllerState>();
+        config = GetComponent<TankConfig>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
