79cec33c2fbe8f9d0f451e010bcc3e77941b7d88
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-11-13 17:06:06 -0500

    tank rotation state

--- Assets/Scripts/Tank/TankControllerState.cs
+++ Assets/Scripts/Tank/TankControllerState.cs
@@ -27,15 +27,29 @@ public class TankControllerState : MonoBehaviour
     public Vector3 velocity;
     public Vector3 angularVelocity;
     public Quaternion rotation;
+    public Quaternion turretRotation;
+    public Quaternion barrelRotation;
 
     public bool invertedThrottle = false;
 
     [System.NonSerialized] private Rigidbody rb;
+    [System.NonSerialized] private Transform turret;
+    [System.NonSerialized] private Transform barrel;
+    [System.NonSerialized] private HingeJoint hingeTurret;
+    [System.NonSerialized] private HingeJoint hingeBarrel;
+    [System.NonSerialized] private Transform rotatorTurret;
+    [System.NonSerialized] private Transform rotatorBarrel;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        turret = transform.Find("body").Find("turret_hinge");
+        barrel = turret.Find("turret_base").Find("turret").Find("barrel_hinge");
+        hingeTurret = turret.GetComponent<HingeJoint>();
+        hingeBarrel = barrel.GetComponent<HingeJoint>();
+        rotatorTurret = hingeTurret.transform.GetChild(0).transform;
+        rotatorBarrel = hingeBarrel.transform.GetChild(0).transform;
     }
 
     bool Ready() {
@@ -65,18 +79,23 @@ public class TankControllerState : MonoBehaviour
     {
         if (!Ready()) return;
 
-        position = transform.position;
-        rotation = transform.rotation;
-        velocity = rb.velocity;
-        angularVelocity = rb.angularVelocity;
-
         if (applyFixedUpdate)
         {
+            // Apply tank state
             applyFixedUpdate = false;
             rb.MovePosition(position + positionOffset);
             rb.MoveRotation(rotation);
             rb.angularVelocity = angularVelocity;
             rb.velocity = velocity;
+            rotatorTurret.localRotation = turretRotation;
+            rotatorBarrel.localRotation = barrelRotation;
         }
+
+        position = transform.position;
+        rotation = transform.rotation;
+        velocity = rb.velocity;
+        angularVelocity = rb.angularVelocity;
+        turretRotation = rotatorTurret.localRotation;
+        barrelRotation = rotatorBarrel.localRotation;
     }
 }
