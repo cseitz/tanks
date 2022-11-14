82f92a93d222135414dbafc64430b66417b6432c
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-11-13 21:57:34 -0500

    copied explosions

--- Assets/Scripts/Entities/Explosions/ExplosionManager.cs
+++ Assets/Scripts/Entities/Explosions/ExplosionManager.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class ExplosionManager : MonoBehaviour
 {
-    
+    [System.Serializable]
+    public struct ExplosionEntry {
+        public string name;
+        public GameObject prefab;
+    }
+
+    public ExplosionEntry[] explosions;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
